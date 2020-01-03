0dfbb4ff105810d16472a2bed7dddf4d5799ba84
olNYXlo <48833626+olNYXlo@users.noreply.github.com>
2020-01-03 16:09:32 +0800

    4.08pm  3/01/2020 completed collision problem statement (A & B separated by 20km. A travels towards B at 40km /hr. B travels towards A at 100km / hr. How many times will they collide before A completes the 20km. B will move towards A & then back to its start position. A only moves towards B)

---
+++ src/testing/Collision.java
@@ -0,0 +1,26 @@
+package testing;
+
+public class Collision {
+
+	public static void main(String[] args) {
+		
+		float distTravelledByA = 0;
+		float distTravelledByB = 0;
+		int count = 0;
+		float totalTime = 0;
+		while (distTravelledByA <20){
+			float time = (20 - distTravelledByA) / 140;
+			distTravelledByA += (40*time);
+			distTravelledByB += (100*time);
+			totalTime += time*2;
+			count ++;
+			System.out.println("Collided " + count + " times");
+			System.out.println("Collided when A travels " + distTravelledByA + " km");
+			System.out.println("Time taken :" + time);
+			System.out.println("Total Time taken : " + totalTime + " hrs");
+			distTravelledByA += (40*time);
+			distTravelledByB -= (100*time);
+		}
+
+	}
+}
